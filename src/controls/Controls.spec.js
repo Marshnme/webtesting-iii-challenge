52cb3971b393bb125f36b92e383426e98aaa7b75
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-10-15 00:04:59 -0400

    button fire tests complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -11,20 +11,33 @@ test('renders Controls correctly', () => {
     });
 
 
-
-    test("buttons provided for toggle", () => {
-        const lockGate = jest.fn();
+    test("close gate button toggle", () => {
         const closeGate = jest.fn();
-        const locked=jest.fn();
         const closed =jest.fn();
-        const {getByText} = render(
-            <Controls toggleLocked={lockGate} toggleClosed={closeGate}
-            closed={closed} locked={locked} disabled={!closed}/>
-        );
+        const locked=jest.fn();
 
+        const {getByText} = render(
+            <Controls  toggleClosed={closeGate}
+            closed={!closed} locked={!locked} />
+        );
         const closeButton = getByText(/close gate/i)
         fireEvent.click(closeButton);
         expect(closeGate).toHaveBeenCalled();
+    })
+
+
+
+    test("lock gate button toggle", () => {
+        const lockGate = jest.fn();
+        const closed =jest.fn();
+        const locked=jest.fn();
+        
+        
+        const {getByText} = render(
+            <Controls toggleLocked={lockGate} 
+            closed={closed} locked={!locked}/>
+        );
+
 
 
             const lockButton = getByText(/lock gate/i);
@@ -32,3 +45,41 @@ test('renders Controls correctly', () => {
         expect(lockGate).toHaveBeenCalled();
 
     })
+
+
+
+    test("open gate button toggle", () => {
+        const openGate = jest.fn();
+        const closed =jest.fn();
+        const locked=jest.fn();
+
+        const {getByText} = render(
+            <Controls  toggleClosed={openGate}
+            closed={closed} locked={!locked} />
+        );
+        const openButton = getByText(/open gate/i)
+        fireEvent.click(openButton);
+        expect(openGate).toHaveBeenCalled();
+    })
+
+
+
+    test("unlock gate button toggle", () => {
+        const unlockGate = jest.fn();
+        const closed =jest.fn();
+        const locked=jest.fn();
+        
+        
+        const {getByText} = render(
+            <Controls toggleLocked={unlockGate} 
+            closed={closed} locked={locked}/>
+        );
+
+
+
+            const unlockButton = getByText(/unlock gate/i);
+        fireEvent.click(unlockButton);
+        expect(unlockGate).toHaveBeenCalled();
+
+    })
+    
